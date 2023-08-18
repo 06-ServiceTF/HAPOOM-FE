6f1ef6ccccd344b2363c4868318eca41223814ea
Haegnim <haegnim@gmail.com>
2023-08-18 17:01:33 +0900

    Fix : Preventing duplicate event invocation

--- hooks/useSwipe.ts
+++ hooks/useSwipe.ts
@@ -9,6 +9,7 @@ const useSwipe = (leftAction: () => void, rightAction: () => void) => {
   const [endX, setEndX] = useState<number | null>(null);
 
   const dragDistance = endX !== null && startX !== null ? endX - startX : 0;
+  console.log(dragDistance);
 
   const calculateDragDistance = useCallback(() => {
     if (dragDistance > 50) {
