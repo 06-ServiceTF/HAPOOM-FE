02ddba22ec6ef137f839661365a0b214afa5313d
SAHA-YJB <pkomom123@naver.com>
2023-08-29 21:01:47 +0900

    feat: self report error message complete

--- redux/reducers/themeSlice.ts
+++ redux/reducers/themeSlice.ts
@@ -1,21 +1,24 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type ThemeMode = 'light' | 'dark';
+type ThemeMode = "light" | "dark";
 
 interface ThemeState {
   mode: ThemeMode;
+  mainTextColor: string;
 }
 
-const initialState = {
-  mode: typeof window !== 'undefined' ? (localStorage.getItem('theme') || 'light') : 'light',
+const initialState: ThemeState = {
+  mode: typeof window !== "undefined" ? (localStorage.getItem("theme") as ThemeMode) || "light" : "light",
+  mainTextColor: typeof window !== "undefined" ? (localStorage.getItem("theme") === "dark" ? "#fff" : "#000") : "#000",
 };
 
 const themeSlice = createSlice({
-  name: 'theme',
+  name: "theme",
   initialState,
   reducers: {
-    setThemeAll: (state, action: PayloadAction<string>) => {
-      return { mode: action.payload };
+    setThemeAll: (state, action: PayloadAction<ThemeMode>) => {
+      state.mode = action.payload;
+      state.mainTextColor = action.payload === "dark" ? "#fff" : "#000";
     },
   },
 });
