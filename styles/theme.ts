d23e6e24140889337763bec9eb9dc7dceffea7f2
Haegnim <haegnim@gmail.com>
2023-08-30 10:48:49 +0900

    Feat : theme mode

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,14 +1,28 @@
 import 'styled-components';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, css } from 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     mode: 'light' | 'dark';
+    mainTextColor: string;
   }
 }
+
+const darkModeStyles = css`
+  background-color: #152b4e;
+  color: #fff;
+  --header-color: #fff;
+`;
+
+const lightModeStyles = css`
+  background-color: #fff;
+  color: #333;
+  --header-color: #2790f6;
+`;
+
 export const ThemeGlobalStyle = createGlobalStyle`
-  body {
-    background-color: ${({ theme }) =>
-      theme.mode === 'dark' ? '#152b4e' : '#fff'};
-  }
+    body {
+      ${({ theme }) =>
+        theme.mode === 'dark' ? darkModeStyles : lightModeStyles}
+    }
 `;
