8de2f1d67f6e8ead2badcf535d3d47e7759e4ae2
Haegnim <haegnim@gmail.com>
2023-09-08 14:52:33 +0900

    test : file

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,32 @@
-// const nextJest = require('next/jest');
+const nextJest = require('next/jest');
 
-// const createJestConfig = nextJest({
-//   dir: './',
-// });
+const createJestConfig = nextJest({
+  dir: './',
+});
 
-// const customJestConfig = {
-//   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-//   moduleNameMapper: {
-//     '^@/(.*)$': '<rootDir>/src/$1',
-//     '^@components/(.*)$': '<rootDir>/src/components/$1',
-//     '^@pages/(.*)$': '<rootDir>/src/pages/$1',
-//     '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-//     '^@public/(.*)$': '<rootDir>/public/$1',
-//     '^next/router$': '<rootDir>/__mocks__/next/router.js',
-//   },
-//   testEnvironment: 'jsdom',
-//   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
-//   transformIgnorePatterns: ['<rootDir>/node_modules/'],
-// };
+const customJestConfig = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/src/next-auth'],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'json',
+    'node',
+  ],
+};
 
-// module.exports = createJestConfig(customJestConfig);
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
